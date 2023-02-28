412842c32a16f8af2bb7b54469ba4b40f9568e0a
Houssam Hamdaoui <hamdaouihoussam.97@gmail.com>
2023-02-28 14:02:53 -0500

    Feat: Finish fillboard function

--- src/gameboard.js
+++ src/gameboard.js
@@ -19,17 +19,42 @@ function Gameboard() {
   }
 
   function fillBoard(shipCoord, newShip) {
+    let i = 0;
+    let coord = shipCoord;
+
+    // check for available space
     if (newShip.orientation === "horizontal") {
       if (newShip.length + shipCoord.x > 10) return 0;
     } else if (newShip.orientation === "vertical") {
       if (newShip.length + shipCoord.y > 10) return 0;
     }
-    while (newShip.length) {
-      if (newShip.orientation === "horizontal")
+
+    // check for free space
+    while (i < newShip.length) {
+      if (newShip.orientation === "horizontal") {
+        if (board[coord.y][coord.x]) {
+          coord.x++;
+          return 0;
+        }
+      }
+      if (newShip.orientation === "vertical") {
+        if (board[coord.y][coord.x]) {
+          coord.y++;
+          return 0;
+        }
+      }
+      i++;
+    }
+
+    i = 0;
+
+    // fill space
+    while (i < newShip.length) {
+      if (newShip.orientation === "horizontal") {
         board[shipCoord.y][shipCoord.x++] = 1;
-      else if (newShip.orientation === "vertical")
+      } else if (newShip.orientation === "vertical")
         board[shipCoord.y++][shipCoord.x] = 1;
-      newShip.length--;
+      i++;
     }
     return 1;
   }
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -110,12 +110,13 @@ describe("Testing the gameboard module", () => {
       ]);
     });
 
-    it.skip("Refuses to fill space if adjacent space is not free with a coordinate and length", () => {
+    it("Refuses to fill space if adjacent space is not free with a coordinate and length", () => {
       let newBoard = Gameboard();
       let shipCoord1 = { x: 0, y: 0 };
       let shipCoord2 = { x: 0, y: 0 };
       newBoard.fillBoard(shipCoord1, Ship(2));
-      newBoard.fillBoard(shipCoord2, Ship(3));
+
+      expect(newBoard.fillBoard(shipCoord2, Ship(3))).toBe(0);
 
       expect(newBoard.getBoard()).toEqual([
         [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -124,6 +125,50 @@ describe("Testing the gameboard module", () => {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+
+    it("Refuses to fill space if adjacent space is not free with a coordinate and length and orientation (horizontal)", () => {
+      let newBoard = Gameboard();
+      let shipCoord1 = { x: 0, y: 0 };
+      let shipCoord2 = { x: 1, y: 0 };
+      newBoard.fillBoard(shipCoord1, Ship(3));
+
+      expect(newBoard.fillBoard(shipCoord2, Ship(3, "horizontal"))).toBe(0);
+
+      expect(newBoard.getBoard()).toEqual([
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ]);
+    });
+
+    it("Refuses to fill space if adjacent space is not free with a coordinate and length and orientation (vertical)", () => {
+      let newBoard = Gameboard();
+      let shipCoord1 = { x: 5, y: 3 };
+      let shipCoord2 = { x: 5, y: 4 };
+      newBoard.fillBoard(shipCoord1, Ship(3, "vertical"));
+
+      expect(newBoard.fillBoard(shipCoord2, Ship(3, "vertical"))).toBe(0);
+
+      expect(newBoard.getBoard()).toEqual([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
